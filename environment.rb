ff7863f6f20b6f8496013f5840cea0a2b52d5ead
root <root@Orkanen.(none)>
2016-02-03 16:11:30 +0100

    smaller changes

---
+++ environment.rb
@@ -0,0 +1,2 @@
+APIURL='heat.vassaro.net/api'
+#APIURL='192.168.0.12:8080/api'
--- schedules/scheduler.rb
+++ schedules/scheduler.rb
@@ -1,11 +1,16 @@
 #!/usr/bin/ruby
 
 scheduler = Rufus::Scheduler.new
-scheduler.every '5m', :first_in => 1 do
+
+scheduler.every '2m', :first_in => 30 do
   Schedule.read
 end
 
-scheduler.every '5m', :first_in => 1 do
+scheduler.every '5m', :first_in => 10 do
   Unit.report
+end
+
+scheduler.every '2m', :first_in => 20 do
+  Unit.get_data
 end
 scheduler.join
